ff08e98fd6887ce1f7e7d0b3371991839eb92876
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-06-17 13:18:25 +0530

    updated

--- train.py
+++ train.py
@@ -2,18 +2,22 @@ from agents import ACAgent, KANACAgent, PPOAgent, KANPPOAgent, DQNAgent
 from agents import Trainer, PPOTrainer, QTrainer
 import gym
 
+
 from lightsim2grid import LightSimBackend
 from grid2op.Action import TopologyChangeAction
 import grid2op
+from grid2op.Parameters import Parameters
 
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-env_name = "rte_case5_example"  # or any other name.
-env = grid2op.make(env_name, test=True, action_class=TopologyChangeAction)
+
+p = Parameters()
+env_name = "rte_case14_realistic"  # or any other name.
+env = grid2op.make(env_name, action_class=TopologyChangeAction, param=p)
 
 
-agent = PPOAgent(n_actions=132, input_dims=182)
+agent = PPOAgent(n_actions=347, input_dims=env.observation_space.n)
 trainer = PPOTrainer(agent=agent, env=env, N=20, n_epochs=50, n_games=100)
 
-trainer.train("result\\ppo.png")
+trainer.train("result\\ppo_case14.png")
 #agentt.save_model("models")
