a37d514c2c8436b65cc411954345518b84c33c0d
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-11-14 15:43:04 +0530

    sparse actor network created

--- agents/ppo.py
+++ agents/ppo.py
@@ -198,6 +198,47 @@ class KANPPOAgent:
 
 
 
+class SparseActorNetwork(nn.Module):
+    def __init__(self, n_actions, input_dims, alpha,
+            fc1_dims=512, fc2_dims=1024, chkpt_dir='models') -> None:
+        super(SparseActorNetwork, self).__init__()
+        self.checkpoint_file = os.path.join(chkpt_dir, 'sparse_actor_torch_ppo_14')
+
+        self.actor = nn.Sequential(
+            nn.Linear(input_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, input_dims),
+            nn.ReLU(),
+            nn.Linear(input_dims, n_actions),
+            nn.Softmax(dim=-1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        dist = self.actor(state)
+        dist = Categorical(dist)
+        
+        return dist
+    
+    def save_checkpoint(self):
+        torch.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(torch.load(self.checkpoint_file))
+
+
+        
 
 class ActorNetwork(nn.Module):
     def __init__(self, n_actions, input_dims, alpha,
